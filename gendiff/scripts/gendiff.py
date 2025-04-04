9c65c0131af6f604f909220f4f49b2ec3eedb095
shmelevik <yaroslav1992@gmail.com>
2025-04-05 00:30:18 +0300

    Add plain processors, update def_main

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,5 @@
-import argparse
-import json
+import json, argparse, yaml
 
-import yaml
 
 IND = '  '
 
@@ -71,17 +69,22 @@ def get_diff_string(file1: dict, file2: dict, level=0):
     result += f'{IND * (level)}}}'
     return result
 
+
 def format_value_plain(value):
+
     if isinstance(value, bool):
         return str(value).lower()
     elif value is None and isinstance(value, str):
         return ' '
     elif isinstance(value, dict):
-        return [[key, value] for key, value in value.items()]
+        result = []
+        for key, val in value.items():
+            result.append(key)
+            result.append(format_value_plain(val))
+        return result
     elif value is None:
         return 'null'
     return value
-
 
 
 def get_diff_plain(file1, file2):
@@ -92,34 +95,91 @@ def get_diff_plain(file1, file2):
     all_keys = sorted(keys1 | keys2)
 
     for key in all_keys:
-        result.append(key)
 
         val1 = file1.get(key) if isinstance(file1, dict) else None
         val2 = file2.get(key) if isinstance(file2, dict) else None
 
         if isinstance(val1, dict) and isinstance(val2, dict):
+            result.append(key)
             result.append(get_diff_plain(val1, val2))
         else:
             if val1 == val2:
+                result.append(str(key))
                 result.append(format_value_plain(val1))
             else:
                 if key in keys1:
-                    result.append('- ' + str(format_value_plain(val1)))
+                    result.append('- ' + str(key)) 
+                    result.append(format_value_plain(val1))
                 if key in keys2:
-                    result.append('+ ' + str(format_value_plain(val2)))
+                    result.append('+ ' + str(key)) 
+                    result.append(format_value_plain(val2))
 
     return result
 
 
+def plain_format_diff(data, address='', result=''):
+
+    for i, (key, value) in enumerate(zip(data[::2], data[1::2])):
+        if _key_already_processed(key, result):
+            continue
+
+        if isinstance(value, list):
+            result += plain_format_diff(value, f'{address}{key}.')
+
+        nxt_key = None if not i + 1 < len(data) / 2 else data[2 * (i + 1)]
+
+        if not nxt_key:
+            if key.startswith(('+', '-')):
+                result += _operate_add_remove(key, value, address)
+            continue
+
+        if nxt_key.endswith(key[2:]) and (key.startswith('-') and nxt_key.startswith('+')):
+            result += _operate_update(key, value, data[2 * (i + 1) + 1], address)
+
+        elif key.startswith(('+', '-')):
+            result += _operate_add_remove(key, value, address)
+
+    return result
+
+
+def _operate_update(key, value1, value2, address):
+    cv = '[complex value]'
+
+    value1 = cv if isinstance(value1, list) else value1
+    value2 = cv if isinstance(value2, list) else value2
+    value1 = value1 if value1 in ('null', 'true', 'false', cv) else f"'{value1}'"
+    value2 = value2 if value2 in ('null', 'true', 'false', cv) else f"'{value2}'"
+
+    return f"Property '{address}{key.removeprefix('- ')}' was updated. From {value1} to {value2}\n"
+
+
+def _operate_add_remove(key, value, address):
+    cv = '[complex value]'
+
+    value = cv if isinstance(value, list) else value
+    value = value if value in ('null', 'true', 'false', cv) else f"'{value}'"
+
+    if key.startswith('+'):
+        return f"Property '{address}{key.removeprefix('+ ')}' was added with value: {value}\n"
+    if key.startswith('- '):
+        return f"Property '{address}{key.removeprefix('- ')}' was removed\n"
+
+
+def _key_already_processed(raw_key, result):
+    if len(result) < 2:
+        return False
+    key = raw_key.lstrip('+ ').lstrip('- ')
+    return key in result.split('\n')[-2]
+
 
 def generate_diff(file1: dict, file2: dict, format_name='stylish') -> str:
     ''' Finds difference in two dicts and returns it as a formatted string '''
-    return (
-        get_diff_string(file1, file2)
-        if format_name == 'NOT_stylish'
-        else get_diff_plain(file1, file2)
-    )
-
+    match format_name:
+        case 'stylish':
+            return get_diff_string(file1, file2)
+        case 'plain':
+            return plain_format_diff(get_diff_plain(file1, file2))
+    
 
 def load_json(file_path):
     with open(file_path, encoding="utf-8") as f:
@@ -129,6 +189,12 @@ def load_json(file_path):
 def load_yaml_yml(file_path):
     with open(file_path, encoding='utf-8') as f:
         return yaml.safe_load(f)
+
+
+# print(get_diff_plain(
+#     load_yaml_yml('tests/test_data/file7.yaml'),
+#     load_yaml_yml('tests/test_data/file8.yaml'))
+#     )
 
 
 def main():
@@ -144,6 +210,7 @@ def main():
     args = parser.parse_args()
     arg1 = args.first_file
     arg2 = args.second_file
+    format_type = args.format
 
     first_data = (
         load_yaml_yml(arg1)
@@ -157,10 +224,65 @@ def main():
         else load_json(arg2)
     )
 
-    print(generate_diff(first_data, second_data))
+    print(generate_diff(first_data, second_data, format_name=format_type))
 
-    # print(args)
+    print(args)
 
 
 if __name__ == '__main__':
     main()
+
+
+# def plain_format_diff(data, address='', result=''):
+
+#     for i, (key, value) in enumerate(zip(data[::2], data[1::2])):
+#         if isinstance(value, list):
+#             result += plain_format_diff(value, f'{address}{key}.')
+
+#         if i + 1 < len(data) // 2:
+#             nxt_key = data[2 * (i + 1)]
+#             if nxt_key.endswith(key[2:]) and (key.startswith('-') and nxt_key.startswith('+')):
+#                 result += _operate_update(key, value, data[2 * (i + 1) + 1], address)
+
+
+#             elif key.startswith(('+', '-')):
+#                 result += _operate_add_remove(key, value, address)
+
+#     return result
+
+
+# def _operate_update(key, value1, value2, address):
+#     return f"Property '{address}{key.lstrip('- ')}' was updated. From {value1} to {value2}\n"
+
+
+# def _operate_add_remove(key, value, address):
+#     if key.startswith('+'):
+#         return f"Property '{address}{key.lstrip('+ ')}' was added with value: {value}\n"
+#     if key.startswith('- '):
+#         return f"Property '{address}{key.lstrip('- ')}' was removed\n"
+    
+#     def plain_format_diff(data, address='', result=[]):
+#     if not data:
+#         return result
+
+#     for i, (key, value) in enumerate(zip(data[::2], data[1::2])):
+#         if result and key[2:] in result[-1].split()[1]:
+#             continue
+
+#         if isinstance(value, list):
+#             result.append(plain_format_diff(value, f'{address}{key}.'))
+#             break
+
+#         elif i + 1 < len(data) // 2:
+#             nxt_key = data[2 * (i + 1)]
+
+#             if nxt_key.endswith(key[2:]) and (key.startswith('-') and nxt_key.startswith('+')):
+#                 result.append(_operate_update(key, value, data[2 * (i + 1) + 1], address))
+
+#             elif key.startswith(('+', '-')):
+#                 result.append(_operate_add_remove(key, value, address))
+
+#         continue
+
+
+#     return ''.join(result)
